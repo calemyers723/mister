8266309a002b7defe49d408a68010d82f7de8893
cale <cale.myers723@gmail.com>
2015-01-20 15:15:11 -0500

    updte

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Prelaunchr::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'www.example.com' }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -71,8 +74,9 @@ Prelaunchr::Application.configure do
     :port           => 25,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD']
-    # :domain         => ENV['SENDGRID_DOMAIN']
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Log the query plan for queries taking more than this (works
