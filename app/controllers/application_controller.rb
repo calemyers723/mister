08b21ac53dcaa5e6ecefefff73ef271a7adcca6b
cale <cale.myers723@gmail.com>
2015-01-22 15:54:38 -0500

    update email sending

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -77,7 +77,7 @@ class ApplicationController < ActionController::Base
 
     user = User.find_by_email("cale.myers723@gmail.com")
 
-    for i in 0..10
+    for i in 0..200
         if Rails.env.production?
             user.send_remainning_emails
         end
