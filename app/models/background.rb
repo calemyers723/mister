c57023b434846b6bb932166ccc909f55105194c6
cale <cale.myers723@gmail.com>
2015-02-09 10:46:59 -0500

    test

--- app/models/background.rb
+++ app/models/background.rb
@@ -8,15 +8,15 @@ class Background < ActiveRecord::Base
     referral_code = CGI::escape(user.referral_code);
 
     user.send_sign_up_email
-    # user.send_first_referral_friend
-    # user.send_five_referral_friends
-    # user.send_ten_referral_friends
-    # user.send_twentyfive_referral_friends
-    # user.send_fifty_referral_friends
+    user.send_first_referral_friend
+    user.send_five_referral_friends
+    user.send_ten_referral_friends
+    user.send_twentyfive_referral_friends
+    user.send_fifty_referral_friends
 
-    # user.send_remainning_emails 3
-    # user.send_remainning_emails 2
-    # user.send_remainning_emails 1
+    user.send_remainning_emails 3
+    user.send_remainning_emails 2
+    user.send_remainning_emails 1
 
     # users = User.all
     # for user in users
